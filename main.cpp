066fb684e47be106c9ccc499aaa07610c592a223
Gaiță Iustin Florian <iustingaita.f@gmail.com>
2023-11-25 16:35:02 +0200

    Nearly finished

--- main.cpp
+++ main.cpp
@@ -5,8 +5,9 @@ using namespace std;
 
 int x, y, fruitX, fruitY, score;
 bool gameOver;
+int tailX[100], tailY[100];
+int nTail;
 enum eDir { STOP = 0, LEFT, RIGHT, UP, DOWN };
-
 eDir dir;
 
 void setup(){
@@ -16,33 +17,44 @@ void setup(){
 }
 
 void frame(int a, int b){
-   system("cls");
-   cout << "Score: " << score;
-   cout << endl;
+    system("cls");
+    cout << "Score: " << score;
+    cout << endl;
 
-   for (int i = 0; i <= a + 1; i++)
-       cout << "#";
-   cout << endl;
+    for (int i = 0; i <= a + 1; i++)
+        cout << "#";
+    cout << endl;
 
-   for (int i = 0; i < b; i++){
-       for (int j = 0; j < a; j++){
-           if (j == 0)
-               cout << "#";
-           if (i == y && j == x)
-               cout << "0";
-           else if (i == fruitY && j == fruitX)
-               cout << "X";
-           else
-               cout << " ";
-           if (j == a-1)
-               cout << "#";
-       }
-       cout << endl;
-   }
+    for (int i = 0; i < b; i++){
+        for (int j = 0; j < a; j++){
+            if (j == 0)
+                cout << "#";
+            if (i == y && j == x)
+                cout << "0";
+            else if (i == fruitY && j == fruitX)
+                cout << "X";
+            {
+                bool print = false;
+                for (int k = 0; k < nTail; k++)
+                {
+                    if (tailX[k] == j && tailY[k] == i)
+                    {
+                        cout << "o";
+                        print = true;
+                    }
+                }
+                if (!print)
+                    cout << " ";
+            }
+            if (j == a-1)
+                cout << "#";
+        }
+        cout << endl;
+    }
 
-   for (int i = 0; i <= a + 1; i++)
-       cout << "#";
-   cout<<endl;
+    for (int i = 0; i <= a + 1; i++)
+        cout << "#";
+    cout<<endl;
 }
 
 void inputControls(){
@@ -68,7 +80,21 @@ void inputControls(){
 }
 
 void inputLogic(int a, int b){
-    switch (dir){
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X, prev2Y;
+    tailX[0] = x;
+    tailY[0] = y;
+    for (int i = 1; i < nTail; i++)
+    {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
+    switch (dir) {
         case LEFT:
             x--;
             break;
@@ -84,15 +110,20 @@ void inputLogic(int a, int b){
         default:
             break;
     }
-    if (x > a || x < 0 || y > b || y < 0)
-        gameOver = true;
+    if (x >= a) x = 0; else if (x < 0) x = a - 1;
+    if (y >= b) y = 0; else if (y < 0) y = b - 1;
+
+    for (int i = 0; i < nTail; i++)
+        if (tailX[i] == x && tailY[i] == y)
+            gameOver = true;
     if (x == fruitX && y == fruitY) {
-        score++;
+        score++; // crestem scorul
         fruitX = rand() % a;
         fruitY = rand() % b;
+        //pozitionam fructul random
+        nTail++;
     }
 }
-
 int main(){
     int width, height;
     cout << "Alegeti nivelul de dificultate: 1(usor) 2(mediu) 3(greu)" << endl;
@@ -119,7 +150,9 @@ int main(){
     fruitY = rand() % height;
     while (!gameOver)
     {
-        frame(width ,height); inputControls(); inputLogic(width,height);
-        Sleep(100);
+        frame(width,height);
+        inputControls();
+        inputLogic(width,height);
+        Sleep(75);
     }
 }
